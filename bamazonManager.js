bbab327266194509ede2b7784e31e502d25b9499
EBayler <erika.bayler@gmail.com>
2019-06-07 18:49:26 -0600

    setting up initial variables and npm requirements

---
+++ bamazonManager.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var chalk = require('chalk');
+var Table = require('cli-table');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '45Vincent98',
+    database: 'bamazon_db'
+});
